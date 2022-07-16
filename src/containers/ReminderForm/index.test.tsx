a1d24ff946167b4094b40e1cfbdd1cfed0e4539d
Henrique Ramos <henriquealberone@outlook.com>
2022-07-16 17:09:14 -0300

    feat: create ReminderForm

---
+++ src/containers/ReminderForm/index.test.tsx
@@ -0,0 +1,137 @@
+import mockReminders from "__mocks__/reminders";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import { format } from "date-fns";
+import _ from "lodash";
+
+import ReminderForm, { ReminderFormProps } from ".";
+
+const renderReminderForm = (props?: Partial<ReminderFormProps>) =>
+  render(<ReminderForm onSubmit={jest.fn()} {...props} />);
+
+describe("<ReminderForm />", () => {
+  it("renders a label input field", () => {
+    const { getByPlaceholderText } = renderReminderForm();
+    expect(getByPlaceholderText("Label")).toBeInTheDocument();
+  });
+
+  it("renders a city input field", () => {
+    const { getByPlaceholderText } = renderReminderForm();
+    expect(getByPlaceholderText("City")).toBeInTheDocument();
+  });
+
+  it("renders a date input field", () => {
+    const { getByPlaceholderText } = renderReminderForm();
+    expect(getByPlaceholderText("Date")).toBeInTheDocument();
+  });
+
+  it("renders a time input field", () => {
+    const { getByPlaceholderText } = renderReminderForm();
+    expect(getByPlaceholderText("Time")).toBeInTheDocument();
+  });
+
+  it("renders a Save Reminder button", () => {
+    const { getByText } = renderReminderForm();
+    expect(getByText("Save Reminder")).toBeInTheDocument();
+  });
+
+  it("calls onSubmit if the form has been successfully submitted", async () => {
+    const onSubmit = jest.fn();
+    const { getByPlaceholderText, getByText } = renderReminderForm({
+      onSubmit,
+    });
+
+    fireEvent.input(getByPlaceholderText("Label"), {
+      target: { value: mockReminders[0].label },
+    });
+    fireEvent.input(getByPlaceholderText("City"), {
+      target: { value: mockReminders[0].city },
+    });
+    fireEvent.input(getByPlaceholderText("Date"), {
+      target: { value: format(mockReminders[0].dateTime, "yyyy-MM-dd") },
+    });
+    fireEvent.input(getByPlaceholderText("Time"), {
+      target: { value: format(mockReminders[0].dateTime, "HH:mm") },
+    });
+
+    fireEvent.click(getByText("Save Reminder"));
+
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalled();
+    });
+  });
+
+  it("sets initial values for the fields if provided", () => {
+    const { getByPlaceholderText } = renderReminderForm({
+      defaultValue: _.omit(mockReminders[0], "id"),
+    });
+
+    expect((getByPlaceholderText("Label") as HTMLInputElement).value).toBe(
+      mockReminders[0].label
+    );
+    expect((getByPlaceholderText("City") as HTMLInputElement).value).toBe(
+      mockReminders[0].city
+    );
+    expect((getByPlaceholderText("Date") as HTMLInputElement).value).toBe(
+      format(mockReminders[0].dateTime, "yyyy-MM-dd")
+    );
+    expect((getByPlaceholderText("Time") as HTMLInputElement).value).toBe(
+      format(mockReminders[0].dateTime, "HH:mm")
+    );
+  });
+
+  describe("validations", () => {
+    it("requires Label to have 30 characters or less", async () => {
+      const { getByPlaceholderText, getByText } = renderReminderForm();
+      fireEvent.input(getByPlaceholderText("Label"), {
+        target: { value: Array.from({ length: 31 }).fill("a").join("") },
+      });
+      fireEvent.blur(getByPlaceholderText("Label"));
+
+      await waitFor(() => {
+        expect(
+          getByText("Label should have 30 characters or less")
+        ).toBeInTheDocument();
+      });
+    });
+
+    it("requires Label field to be filled", async () => {
+      const { getByPlaceholderText, getByText } = renderReminderForm();
+      fireEvent.focus(getByPlaceholderText("Label"));
+      fireEvent.blur(getByPlaceholderText("Label"));
+
+      await waitFor(() => {
+        expect(getByText("Label is required")).toBeInTheDocument();
+      });
+    });
+
+    it("requires City field to be filled", async () => {
+      const { getByPlaceholderText, getByText } = renderReminderForm();
+      fireEvent.focus(getByPlaceholderText("City"));
+      fireEvent.blur(getByPlaceholderText("City"));
+
+      await waitFor(() => {
+        expect(getByText("City is required")).toBeInTheDocument();
+      });
+    });
+
+    it("requires Date field to be filled", async () => {
+      const { getByPlaceholderText, getByText } = renderReminderForm();
+      fireEvent.focus(getByPlaceholderText("Date"));
+      fireEvent.blur(getByPlaceholderText("Date"));
+
+      await waitFor(() => {
+        expect(getByText("Date is required")).toBeInTheDocument();
+      });
+    });
+
+    it("requires Time field to be filled", async () => {
+      const { getByPlaceholderText, getByText } = renderReminderForm();
+      fireEvent.focus(getByPlaceholderText("Time"));
+      fireEvent.blur(getByPlaceholderText("Time"));
+
+      await waitFor(() => {
+        expect(getByText("Time is required")).toBeInTheDocument();
+      });
+    });
+  });
+});
---
+++ src/containers/ReminderForm/index.tsx
@@ -0,0 +1,81 @@
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+
+import Button from "components/Button";
+import TextField from "components/TextField";
+import format from "date-fns/format";
+import _ from "lodash";
+import { Reminder } from "reducers/reminders";
+
+import "./index.scss";
+
+export interface ReminderFormProps {
+  onSubmit: (reminder: FormValues) => void;
+  defaultValue?: Omit<Reminder, "id">;
+}
+
+interface FormValues {
+  label: string;
+  city: string;
+  date: string;
+  time: string;
+}
+
+const ReminderForm: FC<ReminderFormProps> = ({ onSubmit, defaultValue }) => {
+  const { register, formState, handleSubmit } = useForm<FormValues>({
+    mode: "onBlur",
+    defaultValues: defaultValue
+      ? {
+          ..._.omit(defaultValue, "dateTime"),
+          date: format(defaultValue.dateTime, "yyyy-MM-dd"),
+          time: format(defaultValue.dateTime, "HH:mm"),
+        }
+      : undefined,
+  });
+
+  return (
+    <form className="reminder-form" onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        helperText={formState.errors?.label?.message}
+        error={formState.errors?.label !== undefined}
+        placeholder="Label"
+        {...register("label", {
+          required: "Label is required",
+          validate: (value: string) =>
+            value.length < 30 || "Label should have 30 characters or less",
+        })}
+      />
+      <TextField
+        helperText={formState.errors?.city?.message}
+        error={formState.errors?.city !== undefined}
+        placeholder="City"
+        {...register("city", {
+          required: "City is required",
+        })}
+      />
+      <TextField
+        helperText={formState.errors?.date?.message}
+        error={formState.errors?.date !== undefined}
+        type="date"
+        placeholder="Date"
+        {...register("date", {
+          required: "Date is required",
+        })}
+      />
+      <TextField
+        helperText={formState.errors?.time?.message}
+        error={formState.errors?.time !== undefined}
+        placeholder="Time"
+        type="time"
+        {...register("time", {
+          required: "Time is required",
+        })}
+      />
+      <Button className="py-2" type="submit">
+        Save Reminder
+      </Button>
+    </form>
+  );
+};
+
+export default ReminderForm;
