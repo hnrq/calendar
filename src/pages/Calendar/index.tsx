ed1c70031f9505a94acf2dd0c0dc407b5c396b8a
Henrique Ramos <henriquealberone@outlook.com>
2022-07-17 18:41:34 -0300

    feat: generate days of month in Calendar

--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -1,11 +1,88 @@
-import { FC } from "react";
+import { FC, useMemo, useState } from "react";
+import { useSelector } from "react-redux";
+
+import classNames from "classnames";
+import Button from "components/Button";
+import DayOfMonth from "components/DayOfMonth";
+import { format, isSameMonth, isWeekend } from "date-fns";
+import { RootState } from "store/getStore";
+import { getDaysOfCalendarMonth } from "utils/date";
+
+import "./index.scss";
 
 const Calendar: FC = () => {
-  // your calendar implementation Goes here!
-  // Be creative
+  const [date, setDate] = useState({
+    month: new Date().getMonth(),
+    year: new Date().getFullYear(),
+  });
+  const reminders = useSelector((state: RootState) => state.reminders);
+
+  const daysOfMonth = useMemo(
+    () => getDaysOfCalendarMonth(date.month, date.year),
+    [date]
+  );
+
   return (
-    <div className="container">
+    <div className="calendar p-2 container-lg">
       <h1>Calendar</h1>
+      <div className="calendar__year-controls">
+        <Button
+          variant="secondary"
+          onClick={() => setDate((date) => ({ ...date, year: date.year - 1 }))}
+        >
+          <span className="material-icons">navigate_before</span>
+        </Button>
+        <span>{date.year}</span>
+        <Button
+          variant="secondary"
+          onClick={() => setDate((date) => ({ ...date, year: date.year + 1 }))}
+        >
+          <span className="material-icons">navigate_next</span>
+        </Button>
+      </div>
+
+      <div className="calendar__month-controls">
+        <Button
+          variant="secondary"
+          onClick={() =>
+            setDate(({ month, year }) => {
+              if (month === 0) return { month: 11, year: year - 1 };
+              else return { month: month - 1, year };
+            })
+          }
+        >
+          <span className="material-icons">navigate_before</span>
+        </Button>
+        <span>{format(new Date(date.year, date.month), "LLLL")}</span>
+        <Button
+          variant="secondary"
+          onClick={() =>
+            setDate(({ month, year }) => {
+              if (month === 11) return { month: 0, year: year + 1 };
+              else return { month: month + 1, year };
+            })
+          }
+        >
+          <span className="material-icons">navigate_next</span>
+        </Button>
+      </div>
+      <div className="calendar__days-of-month">
+        {daysOfMonth.map((day) => (
+          <DayOfMonth
+            date={day}
+            key={day.getTime()}
+            reminders={reminders[format(day, "yyyy-MM-dd")] ?? []}
+            className={classNames({
+              "day-of-month--weekend": isWeekend(day),
+              "day-of-month--different-month": !isSameMonth(
+                day,
+                new Date(date.year, date.month)
+              ),
+            })}
+            onClick={() => console.log("FaaF")}
+          />
+        ))}
+      </div>
     </div>
   );
 };
